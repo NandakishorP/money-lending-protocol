e007d7f38e568edecf36d96c45d6e76e7ee6f300
Nandakishor22770 <nandakishor22770@gmail.com>
2025-01-01 00:58:09 -0500

    made changes in the testing file

--- test/unit/FinalLendingPoolContract.test.js
+++ test/unit/FinalLendingPoolContract.test.js
@@ -1,76 +1,54 @@
 const { ethers, getNamedAccounts, deployments, network } = require("hardhat");
 const { expect } = require("chai");
 const { BigNumber } = require("ethers");
-
 describe("FinalLendingPoolContract", function () {
     let lendingPoolContract, lpToken, deployer, user1, user2, mockV3Aggregator, usdtToken;
-
     beforeEach(async () => {
         const DECIMALS = 8;
         const INITIAL_PRICE = ethers.utils.parseUnits("2000", DECIMALS);
-
-        // Deploy MockV3Aggregator
         const MockV3Aggregator = await ethers.getContractFactory("MockV3Aggregator");
         mockV3Aggregator = await MockV3Aggregator.deploy(DECIMALS, INITIAL_PRICE);
         await mockV3Aggregator.deployed();
-
-        // Deploy MockUSDTToken
         const MockUSDTToken = await ethers.getContractFactory("MockUSDT");
         usdtToken = await MockUSDTToken.deploy("Tether USD", "USDT", 60);
         await usdtToken.deployed();
-
-        // Mint USDT tokens to lending pool
         const initialUSDTBalance = ethers.utils.parseUnits("1000000", 6);
         [deployer, user1, user2] = await ethers.getSigners();
-
         await usdtToken.mint(deployer.address, initialUSDTBalance);
-
-        // Deploy LPToken
         const LPTokenFactory = await ethers.getContractFactory("LPToken");
         lpToken = await LPTokenFactory.deploy("LP Token", "LPT", {
             gasLimit: 5000000,
             gasPrice: ethers.utils.parseUnits("10", "gwei"),
         });
         await lpToken.deployed();
-
-        // Deploy FinalLendingPoolContract
         const FinalLendingPoolContractFactory = await ethers.getContractFactory("FinalLendingPoolContract");
         lendingPoolContract = await FinalLendingPoolContractFactory.deploy(
             mockV3Aggregator.address,
             usdtToken.address,
             lpToken.address,
             {
-                gasLimit: 5000000, // Adjust based on contract complexity
+                gasLimit: 5000000,
                 gasPrice: ethers.utils.parseUnits("10", "gwei"),
             }
         );
         await lendingPoolContract.deployed();
-
-        // Transfer USDT tokens to the lending pool contract
         await usdtToken.transfer(lendingPoolContract.address, initialUSDTBalance);
-
-        // Set LendingPool in LPToken
         await lpToken.connect(deployer).setLendingPool(lendingPoolContract.address);
     });
-
     it("should deploy FinalLendingPoolContract", async () => {
         expect(lendingPoolContract.address).to.be.properAddress;
     });
-
     it("should deploy LPToken contract", async () => {
         expect(lpToken.address).to.be.properAddress;
     });
-
     it("should correctly initialize lpToken address in the constructor", async () => {
         const storedLPToken = await lendingPoolContract.lpToken();
         expect(storedLPToken).to.equal(lpToken.address);
     });
-
     it("should correctly initialize usdtToken address in the constructor", async () => {
         const storedUsdtToken = await lendingPoolContract.usdtToken();
         expect(storedUsdtToken).to.equal(usdtToken.address);
     });
-
     it("should correctly initialize priceFeed address in the constructor", async () => {
         const storedPriceFeed = await lendingPoolContract.PRICE_FEED();
         expect(storedPriceFeed).to.equal(mockV3Aggregator.address);
@@ -87,16 +65,12 @@ describe("FinalLendingPoolContract", function () {
         const depositValue = await ethers.utils.parseEther("10");
         const tx = await lendingPoolContract.connect(deployer).depositFunds({ value: depositValue });
         await tx.wait();
-
         const deployerDeposit = await lendingPoolContract.getDepositAmount(deployer.address);
         expect(deployerDeposit).to.equal(depositValue);
-
         const totalLiquidity = await lendingPoolContract.totalLiquidity();
         expect(totalLiquidity).to.equal(depositValue);
-
         const lpTokenBalance = await lpToken.balanceOf(deployer.address);
         expect(lpTokenBalance).to.equal(depositValue.div(BigNumber.from(10).pow(18))); // Convert Wei to Ether
-
         await expect(tx)
             .to.emit(lendingPoolContract, "Deposited")
             .withArgs(deployer.address, depositValue);
@@ -134,116 +108,115 @@ describe("FinalLendingPoolContract", function () {
     it("should revert when the requested withdrawal amount exceeds the deposited Ether", async () => {
         const depositValue = ethers.utils.parseEther("5");
         await lendingPoolContract.connect(deployer).depositFunds({ value: depositValue });
-
         const ethWithdrawAmount = ethers.utils.parseEther("10");
         const lpBurnAmount = 1;
-
         await expect(
             lendingPoolContract.connect(deployer).withdrawDeposits(ethWithdrawAmount, lpBurnAmount)
         ).to.be.revertedWith("FinalLendingPoolContract__InsufficientEtherBalance");
     });
-
-
     //testing by burning more lptoken than owned
-
     it("should revert when the LP token burn amount exceeds the user's LP token balance", async () => {
         const depositValue = ethers.utils.parseEther("5");
         await lendingPoolContract.connect(deployer).depositFunds({ value: depositValue });
-
-        const ethWithdrawAmount = ethers.utils.parseEther("2"); // Within deposited Ether
-        const lpBurnAmount = 10; // Exceeds LP token balance
-
+        const ethWithdrawAmount = ethers.utils.parseEther("2");
+        const lpBurnAmount = 10;
         await expect(
             lendingPoolContract.connect(deployer).withdrawDeposits(ethWithdrawAmount, lpBurnAmount)
         ).to.be.revertedWith("FinalLendingPoolContract__InsufficientLPTokens");
     });
-
+    //it tests the main features of the withdraw function
     it("allows users to withdraw the funds they deposit and burn the LP Tokens", async () => {
         const depositValue = await ethers.utils.parseEther("10");
         await lendingPoolContract.connect(deployer).depositFunds({ value: depositValue });
-
         const initialLpTokenBalance = await lpToken.balanceOf(deployer.address);
         expect(initialLpTokenBalance).to.equal(depositValue.div(BigNumber.from(10).pow(18)));
-
         const initialTotalLiquidity = await lendingPoolContract.totalLiquidity();
         expect(initialTotalLiquidity).to.equal(depositValue);
-
         const ethWithdrawAmount = ethers.utils.parseEther("3");
         const lpBurnAmount = 2;
-
         const tx = await lendingPoolContract.connect(deployer).withdrawDeposits(ethWithdrawAmount, lpBurnAmount);
         await tx.wait();
-
         const deployerFinalDeposit = await lendingPoolContract.getDepositAmount(deployer.address);
         expect(deployerFinalDeposit).to.equal(depositValue.sub(ethWithdrawAmount));
-
         const finalLpTokenBalance = await lpToken.balanceOf(deployer.address);
         expect(finalLpTokenBalance).to.equal(initialLpTokenBalance.sub(lpBurnAmount));
-
         await expect(tx)
             .to.emit(lendingPoolContract, "DepositWithdrawn")
             .withArgs(deployer.address, ethWithdrawAmount.add(ethers.utils.parseEther(lpBurnAmount.toString())), lpBurnAmount);
     });
-
+    //it checks the ability of the function to make multiple withdrawals at the same time
+    it("should handle multiple withdrawal requests correctly and emit the event", async () => {
+        const depositValue = ethers.utils.parseEther("10");
+        await lendingPoolContract.connect(deployer).depositFunds({ value: depositValue });
+        const initialLpTokenBalance = await lpToken.balanceOf(deployer.address);
+        const ethWithdrawAmount1 = ethers.utils.parseEther("3");
+        const ethWithdrawAmount2 = ethers.utils.parseEther("2");
+        const lpBurnAmount1 = 1;
+        const lpBurnAmount2 = 3;
+        const tx1 = await lendingPoolContract.connect(deployer).withdrawDeposits(ethWithdrawAmount1, lpBurnAmount1);
+        const receipt1 = await tx1.wait();
+        const totalLiquidityAfterFirstWithdrawal = await lendingPoolContract.totalLiquidity();
+        const lpTokenSupply1 = await lpToken.totalSupply();
+        const lpValueInEth1 = lpBurnAmount1 * totalLiquidityAfterFirstWithdrawal / lpTokenSupply1;
+        expect(receipt1.events).to.exist;
+        expect(receipt1)
+            .to.emit(lendingPoolContract, "DepositWithdrawn")
+            .withArgs(deployer.address, ethWithdrawAmount1.add(ethers.utils.parseEther(lpValueInEth1.toString())), lpBurnAmount1);
+        const tx2 = await lendingPoolContract.connect(deployer).withdrawDeposits(ethWithdrawAmount2, lpBurnAmount2);
+        const receipt2 = await tx2.wait();
+        const totalLiquidityAfterSecondWithdrawal = await lendingPoolContract.totalLiquidity();
+        const lpTokenSupply2 = await lpToken.totalSupply();
+        const lpValueInEth2 = lpBurnAmount2 * totalLiquidityAfterSecondWithdrawal / lpTokenSupply2;
+        expect(receipt2)
+            .to.emit(lendingPoolContract, "DepositWithdrawn")
+            .withArgs(deployer.address, ethWithdrawAmount2.add(ethers.utils.parseEther(lpValueInEth2.toString())), lpBurnAmount2);
+        const deployerFinalDeposit = await lendingPoolContract.getDepositAmount(deployer.address);
+        expect(deployerFinalDeposit).to.equal(
+            depositValue.sub(ethWithdrawAmount1.add(ethWithdrawAmount2))
+        );
+        const finalLpTokenBalance = await lpToken.balanceOf(deployer.address);
+        expect(finalLpTokenBalance).to.equal(initialLpTokenBalance.sub(lpBurnAmount1 + lpBurnAmount2));
+    });
+    //it tests the function to make collateral deposit to borrow funds
     it("allows users to deposit collateral to take loans", async () => {
         const depositValue = await ethers.utils.parseEther("5");
-
         const initalTotalCollateral = await lendingPoolContract.totalCollateral();
         const tx = await lendingPoolContract.connect(user1).depositCollateral({ value: depositValue });
         await tx.wait();
-
         const userCollateralAmount = await lendingPoolContract.getCollateralAmount(user1.address);
         const finalTotalCollateral = await lendingPoolContract.totalCollateral();
-
         expect(depositValue).to.equal(userCollateralAmount);
         expect(finalTotalCollateral).to.equal(initalTotalCollateral.add(depositValue));
-
         await expect(tx)
             .to.emit(lendingPoolContract, "CollateralDeposited")
             .withArgs(user1.address, depositValue);
     });
-
     it("should allow borrowing funds based on collateral and price feed", async () => {
-        const depositCollateralAmount = ethers.utils.parseEther("10"); // 10 ETH
+        const depositCollateralAmount = ethers.utils.parseEther("10");
         await lendingPoolContract.connect(user1).depositCollateral({
             value: depositCollateralAmount,
         });
         const collateralAvailable = await lendingPoolContract.getCollateralAmount(user1.address);
         expect(collateralAvailable).to.equal(depositCollateralAmount);
-
-        const newPrice = ethers.utils.parseUnits("2500", 8); // ETH/USDT = 2500
+        const newPrice = ethers.utils.parseUnits("2500", 8);
         await mockV3Aggregator.updateAnswer(newPrice);
-
-        const borrowAmount = ethers.utils.parseUnits("10000", 6); // 10,000 USDT
-
-        // Approve lending pool to transfer USDT on behalf of user1
+        const borrowAmount = ethers.utils.parseUnits("10000", 6);
         await usdtToken.connect(user1).approve(lendingPoolContract.address, borrowAmount);
-
         const tx = await lendingPoolContract.connect(user1).borrowFunds(borrowAmount);
         await tx.wait();
-
         const userDebt = await lendingPoolContract.getDebt(user1.address);
         expect(userDebt).to.equal(borrowAmount);
-
         const userDebtInETh = await lendingPoolContract.getDebtInETH(user1.address);
-
         await expect(tx)
             .to.emit(lendingPoolContract, "LoanBorrowed")
             .withArgs(user1.address, borrowAmount, userDebtInETh);
     });
     it("should allow borrowing within collateral limits", async function () {
-        // Deposit collateral
-        const depositAmount = ethers.utils.parseEther("10"); // 10 ETH
+        const depositAmount = ethers.utils.parseEther("10");
         await lendingPoolContract.connect(user1).depositCollateral({ value: depositAmount });
-
-        // Check collateral
         const collateralAvailable = await lendingPoolContract.getCollateralAmount(user1.address);
         expect(collateralAvailable).to.equal(depositAmount);
-
-        // Set up borrowing parameters
-        const borrowAmount = ethers.utils.parseUnits("10000", 6); // 10,000 USDT
-
-        // Call borrowFunds
+        const borrowAmount = ethers.utils.parseUnits("10000", 6);
         await expect(lendingPoolContract.connect(user1).borrowFunds(borrowAmount))
             .to.emit(lendingPoolContract, "LoanBorrowed")
             .withArgs(
